1efdd7e247ba70d58bde12480a4b80c2c27b95a8
fedyuk <simone.fedyuk@iisgubbio.edu.it>
2022-12-15 10:55:46 +0100

    riusciamo a contare le richieste 2 a 2

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,7 +1,9 @@
 var http = require('http');
+let contatore=0;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  CONTATORE+=1;
+  res.end(contatore+1+"");
 }).listen(7000); 
-console.log("hello world");
+
