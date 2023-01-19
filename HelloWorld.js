fd88c1788e7b7c9e2b636574c4886f4d8f989f9c
fedyuk <simone.fedyuk@iisgubbio.edu.it>
2023-01-19 11:42:39 +0100

    invio messaggi a entrambi e gestione logout

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,57 +1,64 @@
 
 var http = require('http');
+const { prependListener } = require('process');
 var WebSocketServer = require('websocket').server;
 let personeCollegate = [];
+let persona;
+let listaPersone ="";
 
 var WebSocketServer = require('websocket').server;
 const { client } = require('websocket');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
 
-    response.writeHead(404);
-    response.end();
+  response.writeHead(404);
+  response.end();
 });
-server.listen(8080, function() {
+server.listen(8080, function () {
 });
 
 wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false
+  httpServer: server,
+  autoAcceptConnections: false
 });
 
 function originIsAllowed(origin) {
   return true;
 }
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      request.reject();
-      //sbattuto fori 
-      return;
+wsServer.on('request', function (request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    //sbattuto fori 
+    return;
+  }
+
+  var connection = request.accept(null, request.origin);
+  
+  console.log("si è connesso " + connection.remoteAddress);
+  personeCollegate.push(connection);
+  connection.on('message', function (message) {
+
+    console.log("le persone collegate sono:" + personeCollegate.length);
+
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      for (i = 0; i <= personeCollegate.length - 1; i++) {
+        persona = personeCollegate[i];
+        console.log(i + "===" + persona);
+        persona.sendUTF(message.utf8Data);
+      }
+
     }
-    
-    var connection = request.accept(null, request.origin);
-    console.log("si è connesso "+ connection.remoteAddress);
-    personeCollegate.push(connection);
-    connection.on('message', function(message) {
-    
-    console.log("sssssssssssssssss:"+personeCollegate.length);
-    
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            
-            for(i=0; i<=personeCollegate.length; i++){
-              let persona = personeCollegate[i];
-              console.log(i+" ====================== "+persona);
-              persona.sendUTF(message.utf8Data);
-            }
-            
+    connection.on("close", function () {
+      persona.sendUTF(message.utf8Data);
+      for (let i = 0; i < personeCollegate.length; i++) {
+        if (this == personeCollegate[i]) {
+          personeCollegate.splice(i, 1);          
         }
-        else if (message.type === 'binary') {
-           // console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
+      }
     });
-    
+
+  });
 });
 
